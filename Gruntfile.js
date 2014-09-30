992936236ebbc63dcd0e95e4ea3d275e0ddaa204
jixin <jixin@umeng.com>
2014-09-30 16:35:36 +0800

    add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      main: {
+          files: [
+              {expand: true, flatten: true, cwd: 'script_lib/', src: [
+                  'bootstrap/dist/css/bootstrap.min.css',
+              ], dest: 'stylesheets/script_lib'},
+              {expand: true, flatten: true, cwd: 'script_lib/', src: [
+                  'bootstrap/dist/js/bootstrap.min.js',
+                  'jquery/dist/jquery.min.js',
+                  'jquery/dist/jquery.min.map',
+                  'd3/d3.min.js',
+                  'jquery-qrcode/jquery.qrcode.min.js'
+              ], dest: 'javascripts/script_lib'}
+          ]
+      }
+    }
+  });
+
+  // 加载包含 "uglify" 任务的插件。
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // 默认被执行的任务列表。
+  grunt.registerTask('default', ['copy']);
+
+};
